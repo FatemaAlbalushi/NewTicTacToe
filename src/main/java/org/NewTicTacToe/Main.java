9b6ccee2023be6a48dd5739f41ad1db0628d92bd
FatemaAlbalushi <fatoomsaid188@gmail.com>
2023-02-05 10:00:31 +0400

    Add the main file

---
+++ src/main/java/org/NewTicTacToe/Main.java
@@ -0,0 +1,9 @@
+package org.NewTicTacToe;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Today is Sunday");
+        System.out.println("My name is Fatema");
+    }
+}
